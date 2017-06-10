16ecf8947757d1936880528d3c5a21fbdfdae9cd
Elle <elli.g.pope@gmail.com>
2017-06-10 12:32:46 -0700

    added a function that counts down the dates... right now just a basic countdown. Not very pretty

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 $('#hamburger').on('click', menuDisplay);
 $(window).on('resize', showMenu);
 $(document).ready(divResize);
+$(document).ready(timeToLaunch);
 
 function divResize() {
 	var y = $('article').height();
@@ -22,4 +23,14 @@ function showMenu() {
 
 function menuDisplay() {
 	$('#toggle').slideToggle('slow');
+}
+
+function timeToLaunch() {
+	var launchDate = new Date('2018-02-08 20:00:00 GMT-0700').getTime();
+	var countDownTime = setInterval(function() {
+		var now = new Date().getTime();
+		var distance = launchDate - now;
+		
+		$('#timer-clock').html(distance);
+	});
 }
