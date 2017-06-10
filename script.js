ac5f53774d4178abd7add5c95a72413bfc76eb01
Elle <elli.g.pope@gmail.com>
2017-06-10 13:20:31 -0700

    took the timer out of a function so it is always running and updating the time and always inserts when the document is ready. Along with this created an if/else statement that will change the timer to 'We have launched' for when the date runs out.

--- script.js
+++ script.js
@@ -7,7 +7,30 @@
 $('#hamburger').on('click', menuDisplay);
 $(window).on('resize', showMenu);
 $(document).ready(divResize);
-$(document).ready(timeToLaunch);
+
+//=================================================//
+//This coundown timer is not in a function.
+//This is so the timer is always running
+
+var launchDate = new Date('2018-02-08 20:00:00 GMT-0700').getTime();
+var countDownTime = setInterval(function() {
+	var now = new Date().getTime();
+	var distance = launchDate - now;
+	var msPerDay = 1000 * 60 * 60 * 24;
+	
+	var days = Math.floor(distance / msPerDay);
+ 	var hours = Math.floor((distance % msPerDay) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+ 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	
+	if(distance <= 0) {
+		$('#timer-clock').html('We have Launched!');
+	}else {
+		$('#timer-clock').html(days+'days<br>'+hours+':'+minutes+':'+seconds);
+	}
+});
+
+//=================================================//
 
 function divResize() {
 	var y = $('article').height();
@@ -25,18 +48,4 @@ function menuDisplay() {
 	$('#toggle').slideToggle('slow');
 }
 
-function timeToLaunch() {
-	var launchDate = new Date('2018-02-08 20:00:00 GMT-0700').getTime();
-	var countDownTime = setInterval(function() {
-		var now = new Date().getTime();
-		var distance = launchDate - now;
-		var msPerDay = 1000 * 60 * 60 * 24;
-		
-		var days = Math.floor(distance / msPerDay);
-  	var hours = Math.floor((distance % msPerDay) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-		
-		$('#timer-clock').html(days+'days<br>'+hours+':'+minutes+':'+seconds);
-	});
-}
+
